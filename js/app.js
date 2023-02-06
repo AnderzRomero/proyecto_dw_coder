f11b60e280f39dc8d875dda96c7b7609c1c85b96
Anderson Romero <andersonromerorico@gmail.com>
2023-02-06 01:13:55 -0500

    se realizan modificaciones e implementaciones de librerias y componentes para mejorar el rendimiento de la pagina

---
+++ js/app.js
@@ -0,0 +1,112 @@
+particlesJS(
+  {
+    "particles": {
+      "number": {
+        "value": 80,
+        "density": {
+          "enable": false,
+          "value_area": 315.65905665290904
+        }
+      },
+      "color": {
+        "value": "#797d3c"
+      },
+      "shape": {
+        "type": "star",
+        "stroke": {
+          "width": 1,
+          "color": "#797d3c"
+        },
+        "polygon": {
+          "nb_sides": 5
+        },
+        "image": {
+          "src": "img/github.svg",
+          "width": 30,
+          "height": 30
+        }
+      },
+      "opacity": {
+        "value": 0.04734885849793636,
+        "random": true,
+        "anim": {
+          "enable": true,
+          "speed": 1.2787212787212787,
+          "opacity_min": 0.055944055944055944,
+          "sync": true
+        }
+      },
+      "size": {
+        "value": 3,
+        "random": true,
+        "anim": {
+          "enable": true,
+          "speed": 8,
+          "size_min": 1.5984015984015985,
+          "sync": true
+        }
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 150,
+        "color": "#797d3c",
+        "opacity": 0.29987610382026364,
+        "width": 2.0517838682439087
+      },
+      "move": {
+        "enable": true,
+        "speed": 6.413648243462092,
+        "direction": "none",
+        "random": true,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false,
+        "attract": {
+          "enable": true,
+          "rotateX": 1282.7296486924183,
+          "rotateY": 1282.7296486924183
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "window",
+      "events": {
+        "onhover": {
+          "enable": false,
+          "mode": "grab"
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "push"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 400,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 400,
+          "size": 40,
+          "duration": 2,
+          "opacity": 8,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 200,
+          "duration": 0.4
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true
+  }
+);
